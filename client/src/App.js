427576fcac9b8303e5919e1e914312018fcd9a95
ousselbadri <ousselbadri@gmail.com>
2022-12-23 15:41:05 +0100

    appel dependance app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,28 @@
+// import React, { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LayoutNav from "./Components/layoutNav";
+import {createGlobalStyle} from 'styled-components';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import AdminHome from "./pages/admin/AdminHome";
+// import React, {createContext, useState} from "react";
+import { DataProvider } from "./Components/GlobalState"; 
+// import { userContext } from "./userContext";
+
+
+
+const GlobalStyle = createGlobalStyle
+*{
+  margin: 0;
+  padding: 0;
+  /* background: #2c3e50; */
+      /* background-color: #A5292A; */
+      /* background-color: #B8FAF0; , bleu clair*/
+  font-family: "Open Sans", "sans-serif";
+  overflow-x: hidden
+}
+
 const App = () => {
   return (
     <>
